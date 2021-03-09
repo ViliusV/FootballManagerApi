63b0c95ae526a56af0d71ea15196162585940c73
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-03-09 18:18:10 +0200

    Previous Games controller

---
+++ API/Controllers/PreviousGamesController.cs
@@ -0,0 +1,37 @@
+﻿using API.Utilities;
+using Domain;
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+	[Route("api/team/games/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class PreviousGamesController : ControllerBase
+	{
+		private readonly IGameService _gameService;
+
+		public PreviousGamesController(IGameService gameService)
+		{
+			_gameService = gameService;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Game>> GetAll()
+		{
+			var teamId = HttpContext.User.GetUserTeamId();
+
+			if (teamId == null)
+			{
+				return BadRequest("Current user does not manage a team");
+			}
+
+			var games = _gameService.GetAllPreviousGames(teamId.Value); //ToDo: Return DTO
+
+			return Ok(games);
+		}
+	}
+}
